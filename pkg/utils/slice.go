73b6e71bc4e695e880cf524fdb0dccfd9583534f
Ori Zerah <ori@argon.io>
2022-04-10 11:38:10 +0300

    added useful slice methodfs

--- pkg/utils/slice.go
+++ pkg/utils/slice.go
@@ -15,3 +15,21 @@ func ToSlice[T any](v any) ([]T, bool) {
 	}
 	return result, true
 }
+
+func Map[T any, U any](s []T, cb func(v T) U) []U {
+	result := make([]U, len(s))
+	for i, item := range s {
+		result[i] = cb(item)
+	}
+	return result
+}
+
+func Filter[T any](s []T, cb func(v T) bool) []T {
+	result := make([]T, 0)
+	for _, item := range s {
+		if cb(item) {
+			result = append(result, item)
+		}
+	}
+	return result
+}
