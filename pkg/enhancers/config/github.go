86602d9e898e731d237f57b603f14ab302f3bff5
Ori Zerah <ori@argon.io>
2022-04-26 12:02:50 +0300

    init of enhancers

--- pkg/enhancers/config/github.go
+++ pkg/enhancers/config/github.go
@@ -1,5 +1,18 @@
 package config
 
 var (
-	GithubConfiguration = EnhancementConfiguration{}
+	GithubConfiguration = EnhancementConfiguration{
+		LabelMapping: map[string]ObjectiveConfiguration{
+			"sbom": {
+				Tasks: []string{
+					"argonsecurity/actions/generate-manifest",
+				},
+			},
+			"appsec": {
+				Tasks: []string{
+					"argonsecurity/scanner-action",
+				},
+			},
+		},
+	}
 )
