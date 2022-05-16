520b64aaf1dda1323d257bfae4c3b0006417006f
Ori Zerah <ori@argon.io>
2022-05-16 13:45:33 +0300

    moved tests to blackbox folder

--- test/blackbox/github_test.go
+++ test/blackbox/github_test.go
@@ -1,34 +1,12 @@
-package test
+package blackbox
 
 import (
-	"io/ioutil"
-	"path/filepath"
-	"sort"
 	"testing"
 
-	"github.com/argonsecurity/pipeline-parser/pkg/consts"
-	"github.com/argonsecurity/pipeline-parser/pkg/handler"
 	"github.com/argonsecurity/pipeline-parser/pkg/models"
 	"github.com/argonsecurity/pipeline-parser/pkg/testutils"
 	"github.com/argonsecurity/pipeline-parser/pkg/utils"
-
-	"github.com/go-test/deep"
 )
-
-const (
-	workflowFolderPath = "fixtures/github/"
-)
-
-type TestCase struct {
-	Filename   string
-	Expected   *models.Pipeline
-	ShouldFail bool
-}
-
-func readFile(filename string) []byte {
-	b, _ := ioutil.ReadFile(filename)
-	return b
-}
 
 func getAllGitHubPermissions(permission models.Permission) *models.TokenPermissions {
 	allPermissions := map[string]models.Permission{
@@ -51,7 +29,7 @@ func getAllGitHubPermissions(permission models.Permission) *models.TokenPermissi
 	}
 }
 
-func Test_GitHubParser(t *testing.T) {
+func TestGitHub(t *testing.T) {
 	testCases := []TestCase{
 		{
 			Filename: "steps.yaml",
@@ -392,58 +370,5 @@ func Test_GitHubParser(t *testing.T) {
 			},
 		},
 	}
-
-	for _, testCase := range testCases {
-		buf := readFile(filepath.Join(workflowFolderPath, testCase.Filename))
-		pipeline, err := handler.Handle(buf, consts.GitHubPlatform)
-		if err != nil {
-			if !testCase.ShouldFail {
-				t.Errorf("%s: %s", testCase.Filename, err)
-			}
-			continue
-		}
-
-		if testCase.ShouldFail {
-			t.Errorf("%s: expected error, but got none", testCase.Filename)
-			continue
-		}
-
-		if pipeline.Jobs != nil {
-			pipeline.Jobs = SortJobs(pipeline.Jobs)
-		}
-		if pipeline.Triggers != nil {
-			pipeline.Triggers = &models.Triggers{
-				Triggers:      SortTriggers(pipeline.Triggers.Triggers),
-				FileReference: pipeline.Triggers.FileReference,
-			}
-		}
-
-		if diffs := deep.Equal(pipeline, testCase.Expected); diffs != nil {
-			t.Errorf("there are %d differences in file %s", len(diffs), testCase.Filename)
-			for _, diff := range diffs {
-				t.Errorf(diff)
-			}
-		}
-	}
-}
-
-func SortJobs(jobs *[]models.Job) *[]models.Job {
-	sort.Slice(*jobs, func(i, j int) bool {
-		return *(*jobs)[i].ID < *(*jobs)[j].ID
-	})
-	return jobs
-}
-
-func SortTriggers(triggers []*models.Trigger) []*models.Trigger {
-	sort.Slice(triggers, func(i, j int) bool {
-		return triggers[i].Event < triggers[j].Event
-	})
-	return triggers
-}
-
-func SortParameters(parameters *[]models.Parameter) *[]models.Parameter {
-	sort.Slice(*parameters, func(i, j int) bool {
-		return *(*parameters)[i].Name < *(*parameters)[j].Name
-	})
-	return parameters
+	executeTestCases(t, testCases, "github")
 }
---
+++ test/blackbox/utils.go
@@ -0,0 +1,74 @@
+package blackbox
+
+import (
+	"io/ioutil"
+	"path/filepath"
+	"sort"
+	"testing"
+
+	"github.com/argonsecurity/pipeline-parser/pkg/consts"
+	"github.com/argonsecurity/pipeline-parser/pkg/handler"
+	"github.com/argonsecurity/pipeline-parser/pkg/models"
+	"github.com/go-test/deep"
+)
+
+func readFile(filename string) []byte {
+	b, _ := ioutil.ReadFile(filename)
+	return b
+}
+
+func executeTestCases(t *testing.T, testCases []TestCase, folder string) {
+	for _, testCase := range testCases {
+		buf := readFile(filepath.Join("../fixtures", folder, testCase.Filename))
+		pipeline, err := handler.Handle(buf, consts.GitHubPlatform)
+		if err != nil {
+			if !testCase.ShouldFail {
+				t.Errorf("%s: %s", testCase.Filename, err)
+			}
+			continue
+		}
+
+		if testCase.ShouldFail {
+			t.Errorf("%s: expected error, but got none", testCase.Filename)
+			continue
+		}
+
+		if pipeline.Jobs != nil {
+			pipeline.Jobs = SortJobs(pipeline.Jobs)
+		}
+		if pipeline.Triggers != nil {
+			pipeline.Triggers = &models.Triggers{
+				Triggers:      SortTriggers(pipeline.Triggers.Triggers),
+				FileReference: pipeline.Triggers.FileReference,
+			}
+		}
+
+		if diffs := deep.Equal(pipeline, testCase.Expected); diffs != nil {
+			t.Errorf("there are %d differences in file %s", len(diffs), testCase.Filename)
+			for _, diff := range diffs {
+				t.Errorf(diff)
+			}
+		}
+	}
+}
+
+func SortJobs(jobs *[]models.Job) *[]models.Job {
+	sort.Slice(*jobs, func(i, j int) bool {
+		return *(*jobs)[i].ID < *(*jobs)[j].ID
+	})
+	return jobs
+}
+
+func SortTriggers(triggers []*models.Trigger) []*models.Trigger {
+	sort.Slice(triggers, func(i, j int) bool {
+		return triggers[i].Event < triggers[j].Event
+	})
+	return triggers
+}
+
+func SortParameters(parameters *[]models.Parameter) *[]models.Parameter {
+	sort.Slice(*parameters, func(i, j int) bool {
+		return *(*parameters)[i].Name < *(*parameters)[j].Name
+	})
+	return parameters
+}
