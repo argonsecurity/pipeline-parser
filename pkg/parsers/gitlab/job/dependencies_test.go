7cc77f7934ba1ab6d30ded67361a36570fe0ba8b
tamirkiviti13 <tamir@argon.io>
2022-06-26 11:44:14 +0300

    add gitlab tests

---
+++ pkg/parsers/gitlab/job/dependencies_test.go
@@ -0,0 +1,258 @@
+package job
+
+import (
+	"testing"
+
+	gitlabModels "github.com/argonsecurity/pipeline-parser/pkg/loaders/gitlab/models"
+	"github.com/argonsecurity/pipeline-parser/pkg/loaders/gitlab/models/job"
+	"github.com/argonsecurity/pipeline-parser/pkg/models"
+	"github.com/argonsecurity/pipeline-parser/pkg/utils"
+	"github.com/stretchr/testify/assert"
+)
+
+func TestParseDependencies(t *testing.T) {
+	testCases := []struct {
+		name                    string
+		job                     *gitlabModels.Job
+		expectedJobDependencies []*models.JobDependency
+	}{
+		{
+			name:                    "Job is empty",
+			job:                     &gitlabModels.Job{},
+			expectedJobDependencies: []*models.JobDependency{},
+		},
+		{
+			name: "Job with dependencies and no needs",
+			job: &gitlabModels.Job{
+				Dependencies: []string{"job-1", "job-2", "job-3"},
+			},
+			expectedJobDependencies: []*models.JobDependency{
+				{
+					JobID: utils.GetPtr("job-1"),
+				},
+				{
+					JobID: utils.GetPtr("job-2"),
+				},
+				{
+					JobID: utils.GetPtr("job-3"),
+				},
+			},
+		},
+		{
+			name: "Job with needs no dependencies",
+			job: &gitlabModels.Job{
+				Needs: &job.Needs{
+					{
+						Job:      "job-1",
+						Pipeline: "pipeline-1",
+					},
+					{
+						Job:      "job-2",
+						Pipeline: "pipeline-2",
+					},
+				},
+			},
+			expectedJobDependencies: []*models.JobDependency{
+				{
+					JobID:    utils.GetPtr("job-1"),
+					Pipeline: utils.GetPtr("pipeline-1"),
+				},
+				{
+					JobID:    utils.GetPtr("job-2"),
+					Pipeline: utils.GetPtr("pipeline-2"),
+				},
+			},
+		},
+		{
+			name: "Job with dependencies and needs",
+			job: &gitlabModels.Job{
+				Dependencies: []string{"job-1", "job-2", "job-3"},
+				Needs: &job.Needs{
+					{
+						Job:      "job-1",
+						Pipeline: "pipeline-1",
+					},
+					{
+						Job:      "job-2",
+						Pipeline: "pipeline-2",
+					},
+				},
+			},
+			expectedJobDependencies: []*models.JobDependency{
+				{
+					JobID: utils.GetPtr("job-1"),
+				},
+				{
+					JobID: utils.GetPtr("job-2"),
+				},
+				{
+					JobID: utils.GetPtr("job-3"),
+				},
+				{
+					JobID:    utils.GetPtr("job-1"),
+					Pipeline: utils.GetPtr("pipeline-1"),
+				},
+				{
+					JobID:    utils.GetPtr("job-2"),
+					Pipeline: utils.GetPtr("pipeline-2"),
+				},
+			},
+		},
+	}
+
+	for _, testCase := range testCases {
+		t.Run(testCase.name, func(t *testing.T) {
+			got := parseDependencies(testCase.job)
+
+			assert.ElementsMatch(t, testCase.expectedJobDependencies, got, testCase.name)
+		})
+	}
+}
+
+func TestParseJobDependencies(t *testing.T) {
+	testCases := []struct {
+		name                    string
+		job                     *gitlabModels.Job
+		expectedJobDependencies []*models.JobDependency
+	}{
+		{
+			name:                    "Job is empty",
+			job:                     &gitlabModels.Job{},
+			expectedJobDependencies: []*models.JobDependency{},
+		},
+		{
+			name: "Job dependencies has one element",
+			job: &gitlabModels.Job{
+				Dependencies: []string{"job-1"},
+			},
+			expectedJobDependencies: []*models.JobDependency{
+				{
+					JobID: utils.GetPtr("job-1"),
+				},
+			},
+		},
+		{
+			name: "Job dependencies has some elements",
+			job: &gitlabModels.Job{
+				Dependencies: []string{"job-1", "job-2", "job-3"},
+			},
+			expectedJobDependencies: []*models.JobDependency{
+				{
+					JobID: utils.GetPtr("job-1"),
+				},
+				{
+					JobID: utils.GetPtr("job-2"),
+				},
+				{
+					JobID: utils.GetPtr("job-3"),
+				},
+			},
+		},
+	}
+
+	for _, testCase := range testCases {
+		t.Run(testCase.name, func(t *testing.T) {
+			got := parseJobDependencies(testCase.job)
+
+			assert.ElementsMatch(t, testCase.expectedJobDependencies, got, testCase.name)
+		})
+	}
+}
+
+func TestParseJobNeeds(t *testing.T) {
+	testCases := []struct {
+		name                    string
+		job                     *gitlabModels.Job
+		expectedJobDependencies []*models.JobDependency
+	}{
+		{
+			name:                    "Job is empty",
+			job:                     &gitlabModels.Job{},
+			expectedJobDependencies: []*models.JobDependency{},
+		},
+		{
+			name: "Job needs has one element with job and pipeline",
+			job: &gitlabModels.Job{
+				Needs: &job.Needs{
+					{
+						Job:      "job-1",
+						Pipeline: "pipeline-1",
+					},
+				},
+			},
+			expectedJobDependencies: []*models.JobDependency{
+				{
+					JobID:    utils.GetPtr("job-1"),
+					Pipeline: utils.GetPtr("pipeline-1"),
+				},
+			},
+		},
+		{
+			name: "Job needs has one element with job and no pipeline",
+			job: &gitlabModels.Job{
+				Needs: &job.Needs{
+					{
+						Job:      "job-1",
+						Pipeline: "",
+					},
+				},
+			},
+			expectedJobDependencies: []*models.JobDependency{
+				{
+					JobID:    utils.GetPtr("job-1"),
+					Pipeline: nil,
+				},
+			},
+		},
+		{
+			name: "Job needs has one element with pipeline and no job",
+			job: &gitlabModels.Job{
+				Needs: &job.Needs{
+					{
+						Job:      "",
+						Pipeline: "pipeline-1",
+					},
+				},
+			},
+			expectedJobDependencies: []*models.JobDependency{
+				{
+					JobID:    nil,
+					Pipeline: utils.GetPtr("pipeline-1"),
+				},
+			},
+		},
+		{
+			name: "Job needs has one element with pipeline and no job",
+			job: &gitlabModels.Job{
+				Needs: &job.Needs{
+					{
+						Job:      "",
+						Pipeline: "pipeline-1",
+					},
+					{
+						Job:      "job-1",
+						Pipeline: "pipeline-1",
+					},
+				},
+			},
+			expectedJobDependencies: []*models.JobDependency{
+				{
+					JobID:    nil,
+					Pipeline: utils.GetPtr("pipeline-1"),
+				},
+				{
+					JobID:    utils.GetPtr("job-1"),
+					Pipeline: utils.GetPtr("pipeline-1"),
+				},
+			},
+		},
+	}
+
+	for _, testCase := range testCases {
+		t.Run(testCase.name, func(t *testing.T) {
+			got := parseJobNeeds(testCase.job)
+
+			assert.ElementsMatch(t, testCase.expectedJobDependencies, got, testCase.name)
+		})
+	}
+}
---
+++ pkg/parsers/gitlab/triggers/expressions_test.go
@@ -0,0 +1,73 @@
+package triggers
+
+import (
+	"testing"
+
+	"github.com/stretchr/testify/assert"
+)
+
+func TestIsPositive(t *testing.T) {
+	testCases := []struct {
+		name       string
+		comparison *Comparison
+		expected   bool
+	}{
+		{
+			name:       "Comparison is empty",
+			comparison: &Comparison{},
+			expected:   false,
+		},
+		{
+			name:       "Comparison is equals",
+			comparison: &Comparison{Operator: equals},
+			expected:   true,
+		},
+		{
+			name:       "Comparison is notEquals",
+			comparison: &Comparison{Operator: notEquals},
+			expected:   false,
+		},
+		{
+			name:       "Comparison is match",
+			comparison: &Comparison{Operator: match},
+			expected:   true,
+		},
+		{
+			name:       "Comparison is notMatch",
+			comparison: &Comparison{Operator: notMatch},
+			expected:   false,
+		},
+	}
+
+	for _, testCase := range testCases {
+		t.Run(testCase.name, func(t *testing.T) {
+			got := testCase.comparison.IsPositive()
+
+			assert.Equal(t, testCase.expected, got)
+		})
+	}
+}
+
+// func TestGetComparisons(t *testing.T) {
+// 	testCases := []struct {
+// 		name                string
+// 		expression          string
+// 		expectedComparisons []*Comparison
+// 	}{
+// 		{
+// 			name:                "Expressions is empty",
+// 			expression:          "",
+// 			expectedComparisons: nil,
+// 		},
+// 	}
+
+// 	for _, testCase := range testCases {
+// 		t.Run(testCase.name, func(t *testing.T) {
+// 			got := getComparisons(testCase.expression)
+
+// 			changelog, err := diff.Diff(testCase.expectedComparisons, got)
+// 			assert.NoError(t, err)
+// 			assert.Len(t, changelog, 0)
+// 		})
+// 	}
+// }
