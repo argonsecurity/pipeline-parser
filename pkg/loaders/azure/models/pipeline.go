095b768e820ddb267ff7152a6b38e1e56202a37f
tamirkiviti13 <tamir@argon.io>
2022-07-14 11:13:22 +0300

    add resoures

--- pkg/loaders/azure/models/pipeline.go
+++ pkg/loaders/azure/models/pipeline.go
@@ -13,6 +13,7 @@ type Parameter struct {
 type Pipeline struct {
 	Extends *Extends `yaml:"extends,omitempty"`
 	// Jobs       *Jobs       `yaml:"jobs,omitempty"`
+	// Stages       *Stages       `yaml:"stages,omitempty"`
 	continueOnError bool        `yaml:"continueOnError,omitempty"`
 	Pool            *Pool       `yaml:"pool,omitempty"`
 	Name            string      `yaml:"name,omitempty"`
@@ -20,8 +21,8 @@ type Pipeline struct {
 	Parameters      *Parameters `yaml:"parameters,omitempty"`
 	PR              *PRRef      `yaml:"pr,omitempty"`
 	Schedules       *Schedules  `yaml:"schedules,omitempty"`
-	// Resources *Resources  `yaml:"resources,omitempty"`
-	Steps        *Steps     `yaml:"steps,omitempty"`
-	Variables    *Variables `yaml:"variables,omitempty"`
-	LockBehavior string     `yaml:"lockBehavior,omitempty"`
+	Resources       *Resources  `yaml:"resources,omitempty"`
+	Steps           *Steps      `yaml:"steps,omitempty"`
+	Variables       *Variables  `yaml:"variables,omitempty"`
+	LockBehavior    string      `yaml:"lockBehavior,omitempty"`
 }
--- pkg/loaders/azure/models/resources.go
+++ pkg/loaders/azure/models/resources.go
@@ -1,6 +1,26 @@
 package models
 
-// import "gopkg.in/yaml.v3"
+import (
+	loadersUtils "github.com/argonsecurity/pipeline-parser/pkg/loaders/utils"
+	"github.com/argonsecurity/pipeline-parser/pkg/models"
+	"gopkg.in/yaml.v3"
+)
+
+type Build struct {
+	Build         string `yaml:"build,omitempty"`
+	Type          string `yaml:"type,omitempty"`
+	Connection    string `yaml:"connection,omitempty"`
+	Source        string `yaml:"source,omitempty"`
+	Version       string `yaml:"version,omitempty"`
+	Branch        string `yaml:"branch,omitempty"`
+	Trigger       string `yaml:"trigger,omitempty"`
+	FileReference *models.FileReference
+}
+
+func (b *Build) UnmarshalYAML(node *yaml.Node) error {
+	b.FileReference = loadersUtils.GetFileReference(node)
+	return node.Decode(&b)
+}
 
 type MountReadOnly struct {
 	Work     bool `yaml:"work,omitempty"`
@@ -9,67 +29,111 @@ type MountReadOnly struct {
 	Tasks    bool `yaml:"tasks,omitempty"`
 }
 
-type ContainerTrigger struct {
-	Trigger *Trigger `yaml:"trigger,omitempty"`
+type JobContainer struct {
+	Image           string                   `yaml:"image,omitempty"`
+	Endpoint        string                   `yaml:"endpoint,omitempty"`
+	Env             *EnvironmentVariablesRef `yaml:"env,omitempty"`
+	MapDockerSocket bool                     `yaml:"mapDockerSocket,omitempty"`
+	Options         string                   `yaml:"options,omitempty"`
+	Ports           []string                 `yaml:"ports,omitempty"`
+	Volumes         []string                 `yaml:"volumes,omitempty"`
+	MountReadOnly   *MountReadOnly           `yaml:"mountReadOnly,omitempty"`
 }
 
-// type Build struct {
-// 	Build      string `yaml:"build,omitempty"`
-// 	Type       string `yaml:"type,omitempty"`
-// 	Connection string `yaml:"connection,omitempty"`
-// 	Source     string `yaml:"source,omitempty"`
-// 	Version    string `yaml:"version,omitempty"`
-// 	Branch     string `yaml:"branch,omitempty"`
-// 	Trigger    string `yaml:"trigger,omitempty"`
-// }
-
-type Container struct {
-	Container         string                   `yaml:"container,omitempty"`
-	Image             string                   `yaml:"image,omitempty"`
-	Type              string                   `yaml:"type,omitempty"`
-	Trigger           *ContainerTrigger        `yaml:"trigger,omitempty"`
-	Endpoint          string                   `yaml:"endpoint,omitempty"`
-	Env               *EnvironmentVariablesRef `yaml:"env,omitempty"`
-	MapDockerSocket   bool                     `yaml:"mapDockerSocket,omitempty"`
-	Options           string                   `yaml:"options,omitempty"`
-	Ports             []string                 `yaml:"ports,omitempty"`
-	Volumes           []string                 `yaml:"volumes,omitempty"`
-	MountReadOnly     *MountReadOnly           `yaml:"mountReadOnly,omitempty"`
-	AzureSubscription string                   `yaml:"azureSubscription,omitempty"`
-	ResourceGroup     string                   `yaml:"resourceGroup,omitempty"`
-	Registry          string                   `yaml:"registry,omitempty"`
-	Repository        string                   `yaml:"repository,omitempty"`
+type ResourceContainer struct {
+	Container         string      `yaml:"container,omitempty"`
+	Type              string      `yaml:"type,omitempty"`
+	Trigger           *TriggerRef `yaml:"trigger,omitempty"`
+	AzureSubscription string      `yaml:"azureSubscription,omitempty"`
+	ResourceGroup     string      `yaml:"resourceGroup,omitempty"`
+	Registry          string      `yaml:"registry,omitempty"`
+	Repository        string      `yaml:"repository,omitempty"`
+	JobContainer      `yaml:",inline"`
+	FileReference     *models.FileReference
 }
 
-// type Package struct {
-// 	Package    string `yaml:"package,omitempty"`
-// 	Type       string `yaml:"type,omitempty"`
-// 	Connection string `yaml:"connection,omitempty"`
-// 	Name       string `yaml:"name,omitempty"`
-// 	Version    string `yaml:"version,omitempty"`
-// 	Tag        string `yaml:"tag,omitempty"`
-// 	Trigger    string `yaml:"trigger,omitempty"`
-// }
+func (c *ResourceContainer) UnmarshalYAML(node *yaml.Node) error {
+	c.FileReference = loadersUtils.GetFileReference(node)
+	return node.Decode(&c)
+}
 
-// type ResourcePipeline struct {
-// 	Pipeline string   `yaml:"pipeline,omitempty"`
-// 	Project  string   `yaml:"project,omitempty"`
-// 	Source   string   `yaml:"source,omitempty"`
-// 	Version  string   `yaml:"version,omitempty"`
-// 	Branch   string   `yaml:"branch,omitempty"`
-// 	Tags     []string `yaml:"tags,omitempty"`
-// }
+type ResourcePipeline struct {
+	Pipeline string      `yaml:"pipeline,omitempty"`
+	Project  string      `yaml:"project,omitempty"`
+	Source   string      `yaml:"source,omitempty"`
+	Version  string      `yaml:"version,omitempty"`
+	Branch   string      `yaml:"branch,omitempty"`
+	Tags     []string    `yaml:"tags,omitempty"`
+	Trigger  *TriggerRef `yaml:"trigger,omitempty"`
 
-// type Resources struct {
-// 	Builds     *[]Build     `yaml:"builds,omitempty"`
-// 	Containers *[]Container `yaml:"containers,omitempty"`
-// 	Packages   *[]Package   `yaml:"packages,omitempty"`
-// }
+	FileReference *models.FileReference
+}
 
-// func (r *Resources) UnmarshalYAML(node *yaml.Node) error {
-// 	return node.Decode(r)
-// }
+func (p *ResourcePipeline) UnmarshalYAML(node *yaml.Node) error {
+	p.FileReference = loadersUtils.GetFileReference(node)
+	return node.Decode(&p)
+}
 
-// func (r *ContainerTrigger) UnmarshalYAML(node *yaml.Node) error {
-// 	return node.Decode(r)
-// }
+type Repository struct {
+	Repository    string      `yaml:"repository,omitempty"`
+	Endpoint      string      `yaml:"endpoint,omitempty"`
+	Trigger       *TriggerRef `yaml:"trigger,omitempty"`
+	Name          string      `yaml:"name,omitempty"`
+	Type          string      `yaml:"type,omitempty"`
+	Ref           string      `yaml:"ref,omitempty"`
+	FileReference *models.FileReference
+}
+
+func (r *Repository) UnmarshalYAML(node *yaml.Node) error {
+	r.FileReference = loadersUtils.GetFileReference(node)
+	return node.Decode(&r)
+}
+
+type Path struct {
+	Path  string `yaml:"path,omitempty"`
+	Value string `yaml:"value,omitempty"`
+}
+
+type Webhook struct {
+	Webhook       string `yaml:"webhook,omitempty"`
+	Connection    string `yaml:"connection,omitempty"`
+	Type          string `yaml:"type,omitempty"`
+	Filters       []Path
+	FileReference *models.FileReference
+}
+
+func (w *Webhook) UnmarshalYAML(node *yaml.Node) error {
+	w.FileReference = loadersUtils.GetFileReference(node)
+	return node.Decode(&w)
+}
+
+type Package struct {
+	Package       string `yaml:"package,omitempty"`
+	Type          string `yaml:"type,omitempty"`
+	Connection    string `yaml:"connection,omitempty"`
+	Name          string `yaml:"name,omitempty"`
+	Version       string `yaml:"version,omitempty"`
+	Tag           string `yaml:"tag,omitempty"`
+	Trigger       string `yaml:"trigger,omitempty"`
+	FileReference *models.FileReference
+}
+
+func (p *Package) UnmarshalYAML(node *yaml.Node) error {
+	p.FileReference = loadersUtils.GetFileReference(node)
+	return node.Decode(&p)
+}
+
+type Resources struct {
+	Builds        *[]Build             `yaml:"builds,omitempty"`
+	Containers    *[]ResourceContainer `yaml:"containers,omitempty"`
+	Pipelines     *[]ResourcePipeline  `yaml:"pipelines,omitempty"`
+	Repositories  *[]Repository        `yaml:"repositories,omitempty"`
+	Webhooks      *[]Webhook           `yaml:"webhooks,omitempty"`
+	Packages      *[]Package           `yaml:"packages,omitempty"`
+	FileReference *models.FileReference
+}
+
+func (r *Resources) UnmarshalYAML(node *yaml.Node) error {
+	r.FileReference = loadersUtils.GetFileReference(node)
+	return node.Decode(&r)
+}
