d67f0e9f7361d9ec214428b8fafeaa971bd5e400
Ori <orizerahmc@gmail.com>
2022-08-11 14:43:31 +0300

    all parser tests use testutils.DeepCompare

--- pkg/parsers/utils/map_test.go
+++ pkg/parsers/utils/map_test.go
@@ -5,7 +5,7 @@ import (
 
 	loadersCommonModels "github.com/argonsecurity/pipeline-parser/pkg/loaders/common/models"
 	"github.com/argonsecurity/pipeline-parser/pkg/models"
-	testUtils "github.com/argonsecurity/pipeline-parser/pkg/testutils"
+	"github.com/argonsecurity/pipeline-parser/pkg/testutils"
 	"github.com/argonsecurity/pipeline-parser/pkg/utils"
 )
 
@@ -27,36 +27,36 @@ func TestParseMapToParameters(t *testing.T) {
 					{
 						Key:           "string",
 						Value:         "string",
-						FileReference: testUtils.CreateFileReference(112, 224, 112, 238),
+						FileReference: testutils.CreateFileReference(112, 224, 112, 238),
 					},
 					{
 						Key:           "int",
 						Value:         1,
-						FileReference: testUtils.CreateFileReference(113, 224, 113, 230),
+						FileReference: testutils.CreateFileReference(113, 224, 113, 230),
 					},
 					{
 						Key:           "bool",
 						Value:         true,
-						FileReference: testUtils.CreateFileReference(114, 224, 114, 234),
+						FileReference: testutils.CreateFileReference(114, 224, 114, 234),
 					},
 				},
-				FileReference: testUtils.CreateFileReference(111, 222, 333, 444),
+				FileReference: testutils.CreateFileReference(111, 222, 333, 444),
 			},
 			expectedParameters: []*models.Parameter{
 				{
 					Name:          utils.GetPtr("string"),
 					Value:         "string",
-					FileReference: testUtils.CreateFileReference(112, 224, 112, 238),
+					FileReference: testutils.CreateFileReference(112, 224, 112, 238),
 				},
 				{
 					Name:          utils.GetPtr("int"),
 					Value:         1,
-					FileReference: testUtils.CreateFileReference(113, 224, 113, 230),
+					FileReference: testutils.CreateFileReference(113, 224, 113, 230),
 				},
 				{
 					Name:          utils.GetPtr("bool"),
 					Value:         true,
-					FileReference: testUtils.CreateFileReference(114, 224, 114, 234),
+					FileReference: testutils.CreateFileReference(114, 224, 114, 234),
 				},
 			},
 		},
@@ -65,7 +65,7 @@ func TestParseMapToParameters(t *testing.T) {
 	for _, testCase := range testCases {
 		t.Run(testCase.name, func(t *testing.T) {
 			got := ParseMapToParameters(testCase.with)
-			testUtils.DeepCompare(t, testCase.expectedParameters, got)
+			testutils.DeepCompare(t, testCase.expectedParameters, got)
 		})
 	}
 }
