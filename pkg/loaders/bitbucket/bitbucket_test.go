45a487773b49d7ef66511490b211793651b2b397
Daniel Ciuraru <danielc@argon.io>
2023-01-12 16:53:16 +0200

    fix(): test

--- pkg/loaders/bitbucket/bitbucket_test.go
+++ pkg/loaders/bitbucket/bitbucket_test.go
@@ -21,230 +21,230 @@ func TestLoad(t *testing.T) {
 		expectedPipeline *bbModels.Pipeline
 		expectedError    error
 	}{
-		// {
-		// 	name:     "parallel steps",
-		// 	filename: "../../../test/fixtures/bitbucket/parallel-steps.yml",
-		// 	expectedPipeline: &bbModels.Pipeline{
-		// 		Image: &bbModels.Image{
-		// 			Name: "node:16",
-		// 		},
-		// 		Pipelines: &bbModels.BuildPipelines{
-		// 			Default: []*bbModels.Step{
-		// 				{
-		// 					Parallel: []*bbModels.ParallelSteps{
-		// 						{
-		// 							Step: &bbModels.ExecutionUnitRef{
-		// 								ExecutionUnit: &bbModels.ExecutionUnit{
-		// 									Name: "Build and Test",
-		// 									Caches: []string{
-		// 										"node",
-		// 									},
-		// 									Script: []bbModels.Script{
-		// 										{
-		// 											String:        "npm install",
-		// 											FileReference: testutils.CreateFileReference(11, 17, 11, 28),
-		// 										},
-		// 										{
-		// 											String:        "npm test",
-		// 											FileReference: testutils.CreateFileReference(12, 17, 12, 25),
-		// 										},
-		// 									},
-		// 								},
-		// 								FileReference: testutils.CreateFileReference(7, 13, 12, 25),
-		// 							},
-		// 						},
-		// 						{
-		// 							Step: &bbModels.ExecutionUnitRef{
-		// 								ExecutionUnit: &bbModels.ExecutionUnit{
-		// 									Name: "Code linting",
-		// 									Caches: []string{
-		// 										"node",
-		// 									},
-		// 									Script: []bbModels.Script{
-		// 										{
-		// 											String:        "npm install eslint",
-		// 											FileReference: testutils.CreateFileReference(16, 17, 16, 35),
-		// 										},
-		// 										{
-		// 											String:        "npx eslint .",
-		// 											FileReference: testutils.CreateFileReference(17, 17, 17, 29),
-		// 										},
-		// 									},
-		// 								},
-		// 								FileReference: testutils.CreateFileReference(14, 13, 19, 21),
-		// 							},
-		// 						},
-		// 					},
-		// 				},
-		// 			},
-		// 		},
-		// 	},
-		// 	expectedError: nil,
-		// },
-		// {
-		// 	name:     "sync steps",
-		// 	filename: "../../../test/fixtures/bitbucket/sync-steps.yml",
-		// 	expectedPipeline: &bbModels.Pipeline{
-		// 		Image: &bbModels.Image{
-		// 			Name: "node:16",
-		// 		},
-		// 		Pipelines: &bbModels.BuildPipelines{
-		// 			PullRequests: &bbModels.StepMap{
-		// 				"**": {
-		// 					{
-		// 						Step: &bbModels.ExecutionUnitRef{
-		// 							ExecutionUnit: &bbModels.ExecutionUnit{
-		// 								Name: "Build and Test",
-		// 								Caches: []string{
-		// 									"node",
-		// 								},
-		// 								Script: []bbModels.Script{
-		// 									{
-		// 										String:        "npm install",
-		// 										FileReference: testutils.CreateFileReference(11, 15, 11, 26),
-		// 									},
-		// 									{
-		// 										String:        "npm test",
-		// 										FileReference: testutils.CreateFileReference(12, 15, 12, 23),
-		// 									},
-		// 								},
-		// 							},
-		// 							FileReference: testutils.CreateFileReference(6, 9, 12, 23),
-		// 						},
-		// 					},
-		// 					{
-		// 						Step: &bbModels.ExecutionUnitRef{
-		// 							ExecutionUnit: &bbModels.ExecutionUnit{
-		// 								Name: "Code linting",
-		// 								Caches: []string{
-		// 									"node",
-		// 								},
-		// 								Script: []bbModels.Script{
-		// 									{
-		// 										String:        "npm install eslint",
-		// 										FileReference: testutils.CreateFileReference(16, 15, 16, 33),
-		// 									},
-		// 									{
-		// 										String:        "npx eslint .",
-		// 										FileReference: testutils.CreateFileReference(17, 15, 17, 27),
-		// 									},
-		// 								},
-		// 							},
-		// 							FileReference: testutils.CreateFileReference(13, 9, 19, 19),
-		// 						},
-		// 					},
-		// 				},
-		// 			},
-		// 		},
-		// 	},
-		// 	expectedError: nil,
-		// },
-		// {
-		// 	name:     "multiple pipelines definitions",
-		// 	filename: "../../../test/fixtures/bitbucket/multiple-pipelines-types.yml",
-		// 	expectedPipeline: &bbModels.Pipeline{
-		// 		Image: &bbModels.Image{
-		// 			Name: "node:16",
-		// 		},
-		// 		Pipelines: &bbModels.BuildPipelines{
-		// 			Custom: &bbModels.StepMap{
-		// 				"notify": {
-		// 					{
-		// 						Step: &bbModels.ExecutionUnitRef{
-		// 							ExecutionUnit: &bbModels.ExecutionUnit{
-		// 								Name: "Notify Teams",
-		// 								Caches: []string{
-		// 									"node",
-		// 								},
-		// 								Script: []bbModels.Script{
-		// 									{
-		// 										String:        "npx notify -s \"deployment\"",
-		// 										FileReference: testutils.CreateFileReference(11, 15, 11, 41),
-		// 									},
-		// 								},
-		// 							},
-		// 							FileReference: testutils.CreateFileReference(6, 9, 11, 41),
-		// 						},
-		// 					},
-		// 				},
-		// 			},
-		// 			Branches: &bbModels.StepMap{
-		// 				"master": {
-		// 					{
-		// 						Step: &bbModels.ExecutionUnitRef{
-		// 							ExecutionUnit: &bbModels.ExecutionUnit{
-		// 								Name: "step 1",
-		// 							},
-		// 							FileReference: testutils.CreateFileReference(14, 9, 15, 23),
-		// 						},
-		// 					},
-		// 					{
-		// 						Step: &bbModels.ExecutionUnitRef{
-		// 							ExecutionUnit: &bbModels.ExecutionUnit{
-		// 								Name: "step 2",
-		// 							},
-		// 							FileReference: testutils.CreateFileReference(16, 9, 17, 23),
-		// 						},
-		// 					},
-		// 					{
-		// 						Parallel: []*bbModels.ParallelSteps{
-		// 							{
-		// 								Step: &bbModels.ExecutionUnitRef{
-		// 									ExecutionUnit: &bbModels.ExecutionUnit{
-		// 										Name: "step 3",
-		// 									},
-		// 									FileReference: testutils.CreateFileReference(20, 15, 20, 27),
-		// 								},
-		// 							},
-		// 							{
-		// 								Step: &bbModels.ExecutionUnitRef{
-		// 									ExecutionUnit: &bbModels.ExecutionUnit{
-		// 										Name: "step 4",
-		// 									},
-		// 									FileReference: testutils.CreateFileReference(22, 15, 22, 27),
-		// 								},
-		// 							},
-		// 						},
-		// 					},
-		// 				},
-		// 			},
-		// 		},
-		// 	},
-		// 	expectedError: nil,
-		// },
-		// {
-		// 	name:     "step with image",
-		// 	filename: "../../../test/fixtures/bitbucket/image.yml",
-		// 	expectedPipeline: &bbModels.Pipeline{
-		// 		Image: &bbModels.Image{
-		// 			Name: "node:16",
-		// 		},
-		// 		Pipelines: &bbModels.BuildPipelines{
-		// 			Default: []*bbModels.Step{
-		// 				{
-		// 					Step: &bbModels.ExecutionUnitRef{
-		// 						ExecutionUnit: &bbModels.ExecutionUnit{
-		// 							Image: &bbModels.Image{
-		// 								ImageWithCustomUser: &bbModels.ImageWithCustomUser{
-		// 									Name:     "node:16",
-		// 									Email:    "test@test.com",
-		// 									Username: "test",
-		// 									Password: "test",
-		// 									Aws: &bbModels.Aws{
-		// 										AccessKey: "123456",
-		// 										SecretKey: "7891011",
-		// 									},
-		// 								},
-		// 							},
-		// 						},
-		// 						FileReference: testutils.CreateFileReference(5, 7, 13, 32),
-		// 					},
-		// 				},
-		// 			},
-		// 		},
-		// 	},
-		// 	expectedError: nil,
-		// },
+		{
+			name:     "parallel steps",
+			filename: "../../../test/fixtures/bitbucket/parallel-steps.yml",
+			expectedPipeline: &bbModels.Pipeline{
+				Image: &bbModels.Image{
+					Name: "node:16",
+				},
+				Pipelines: &bbModels.BuildPipelines{
+					Default: []*bbModels.Step{
+						{
+							Parallel: []*bbModels.ParallelSteps{
+								{
+									Step: &bbModels.ExecutionUnitRef{
+										ExecutionUnit: &bbModels.ExecutionUnit{
+											Name: "Build and Test",
+											Caches: []string{
+												"node",
+											},
+											Script: []bbModels.Script{
+												{
+													String:        "npm install",
+													FileReference: testutils.CreateFileReference(11, 17, 11, 28),
+												},
+												{
+													String:        "npm test",
+													FileReference: testutils.CreateFileReference(12, 17, 12, 25),
+												},
+											},
+										},
+										FileReference: testutils.CreateFileReference(7, 13, 12, 25),
+									},
+								},
+								{
+									Step: &bbModels.ExecutionUnitRef{
+										ExecutionUnit: &bbModels.ExecutionUnit{
+											Name: "Code linting",
+											Caches: []string{
+												"node",
+											},
+											Script: []bbModels.Script{
+												{
+													String:        "npm install eslint",
+													FileReference: testutils.CreateFileReference(16, 17, 16, 35),
+												},
+												{
+													String:        "npx eslint .",
+													FileReference: testutils.CreateFileReference(17, 17, 17, 29),
+												},
+											},
+										},
+										FileReference: testutils.CreateFileReference(14, 13, 19, 21),
+									},
+								},
+							},
+						},
+					},
+				},
+			},
+			expectedError: nil,
+		},
+		{
+			name:     "sync steps",
+			filename: "../../../test/fixtures/bitbucket/sync-steps.yml",
+			expectedPipeline: &bbModels.Pipeline{
+				Image: &bbModels.Image{
+					Name: "node:16",
+				},
+				Pipelines: &bbModels.BuildPipelines{
+					PullRequests: &bbModels.StepMap{
+						"**": {
+							{
+								Step: &bbModels.ExecutionUnitRef{
+									ExecutionUnit: &bbModels.ExecutionUnit{
+										Name: "Build and Test",
+										Caches: []string{
+											"node",
+										},
+										Script: []bbModels.Script{
+											{
+												String:        "npm install",
+												FileReference: testutils.CreateFileReference(11, 15, 11, 26),
+											},
+											{
+												String:        "npm test",
+												FileReference: testutils.CreateFileReference(12, 15, 12, 23),
+											},
+										},
+									},
+									FileReference: testutils.CreateFileReference(6, 9, 12, 23),
+								},
+							},
+							{
+								Step: &bbModels.ExecutionUnitRef{
+									ExecutionUnit: &bbModels.ExecutionUnit{
+										Name: "Code linting",
+										Caches: []string{
+											"node",
+										},
+										Script: []bbModels.Script{
+											{
+												String:        "npm install eslint",
+												FileReference: testutils.CreateFileReference(16, 15, 16, 33),
+											},
+											{
+												String:        "npx eslint .",
+												FileReference: testutils.CreateFileReference(17, 15, 17, 27),
+											},
+										},
+									},
+									FileReference: testutils.CreateFileReference(13, 9, 19, 19),
+								},
+							},
+						},
+					},
+				},
+			},
+			expectedError: nil,
+		},
+		{
+			name:     "multiple pipelines definitions",
+			filename: "../../../test/fixtures/bitbucket/multiple-pipelines-types.yml",
+			expectedPipeline: &bbModels.Pipeline{
+				Image: &bbModels.Image{
+					Name: "node:16",
+				},
+				Pipelines: &bbModels.BuildPipelines{
+					Custom: &bbModels.StepMap{
+						"notify": {
+							{
+								Step: &bbModels.ExecutionUnitRef{
+									ExecutionUnit: &bbModels.ExecutionUnit{
+										Name: "Notify Teams",
+										Caches: []string{
+											"node",
+										},
+										Script: []bbModels.Script{
+											{
+												String:        "npx notify -s \"deployment\"",
+												FileReference: testutils.CreateFileReference(11, 15, 11, 41),
+											},
+										},
+									},
+									FileReference: testutils.CreateFileReference(6, 9, 11, 41),
+								},
+							},
+						},
+					},
+					Branches: &bbModels.StepMap{
+						"master": {
+							{
+								Step: &bbModels.ExecutionUnitRef{
+									ExecutionUnit: &bbModels.ExecutionUnit{
+										Name: "step 1",
+									},
+									FileReference: testutils.CreateFileReference(14, 9, 15, 23),
+								},
+							},
+							{
+								Step: &bbModels.ExecutionUnitRef{
+									ExecutionUnit: &bbModels.ExecutionUnit{
+										Name: "step 2",
+									},
+									FileReference: testutils.CreateFileReference(16, 9, 17, 23),
+								},
+							},
+							{
+								Parallel: []*bbModels.ParallelSteps{
+									{
+										Step: &bbModels.ExecutionUnitRef{
+											ExecutionUnit: &bbModels.ExecutionUnit{
+												Name: "step 3",
+											},
+											FileReference: testutils.CreateFileReference(20, 15, 20, 27),
+										},
+									},
+									{
+										Step: &bbModels.ExecutionUnitRef{
+											ExecutionUnit: &bbModels.ExecutionUnit{
+												Name: "step 4",
+											},
+											FileReference: testutils.CreateFileReference(22, 15, 22, 27),
+										},
+									},
+								},
+							},
+						},
+					},
+				},
+			},
+			expectedError: nil,
+		},
+		{
+			name:     "step with image",
+			filename: "../../../test/fixtures/bitbucket/image.yml",
+			expectedPipeline: &bbModels.Pipeline{
+				Image: &bbModels.Image{
+					Name: "node:16",
+				},
+				Pipelines: &bbModels.BuildPipelines{
+					Default: []*bbModels.Step{
+						{
+							Step: &bbModels.ExecutionUnitRef{
+								ExecutionUnit: &bbModels.ExecutionUnit{
+									Image: &bbModels.Image{
+										ImageWithCustomUser: &bbModels.ImageWithCustomUser{
+											Name:     "node:16",
+											Email:    Ptr("test@test.com"),
+											Username: Ptr("test"),
+											Password: Ptr("test"),
+											Aws: &bbModels.Aws{
+												AccessKey: "123456",
+												SecretKey: "7891011",
+											},
+										},
+									},
+								},
+								FileReference: testutils.CreateFileReference(5, 7, 13, 32),
+							},
+						},
+					},
+				},
+			},
+			expectedError: nil,
+		},
 		{
 			name:     "additional config: clone, options, services, variables",
 			filename: "../../../test/fixtures/bitbucket/config-options.yml",
