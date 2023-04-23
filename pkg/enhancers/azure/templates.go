12503c56c4c5cb1a2fa217e41c8765b13afc4f55
Daniel Ciuraru <danielc@argon.io>
2023-04-23 13:22:26 +0300

    debug(): url

--- pkg/enhancers/azure/templates.go
+++ pkg/enhancers/azure/templates.go
@@ -93,6 +93,7 @@ func loadRemoteFile(jobImport *models.Import, resources *models.Resources, crede
 		*baseUrl = AZURE_SAAS_BASE_URL
 	}
 	url := generateRequestUrl(project, repo, path, version, *organization, *baseUrl)
+	fmt.Println(url)
 	client := utils.GetHttpClientWithBasicAuth(credentials)
 	resp, err := client.R().Get(url)
 	if err != nil {
